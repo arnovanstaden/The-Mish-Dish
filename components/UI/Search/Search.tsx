162d8ed6647f765378a82cffdf003827852258ba
Arno van Staden <arnovanstaden@gmail.com>
2021-03-09 12:25:57 +0200

    update search

--- components/UI/Search/Search.tsx
+++ components/UI/Search/Search.tsx
@@ -1,10 +1,22 @@
+import { useRouter } from 'next/router'
+
+// Styles
 import styles from "./search.module.scss";
 
 export default function Search() {
+    const router = useRouter()
+
+    const submitSearch = (e) => {
+        if (e.key === "Enter") {
+            const searchTerm = document.getElementById("search-bar") as HTMLInputElement;
+            router.push(`/recipes/?${searchTerm.value}`)
+        }
+    }
+
     return (
         <div className={styles.search}>
             <i className="icon-search"></i>
-            <input type="text" placeholder="What are you in the mood for?" />
+            <input id="search-bar" type="text" placeholder="What are you in the mood for?" onKeyDown={(e) => submitSearch(e)} />
         </div>
     )
 }
