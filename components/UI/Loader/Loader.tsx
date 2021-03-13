04a2a9519ed2f0ce4bce95d150af1930b2b9ed8b
Arno van Staden <arnovanstaden@gmail.com>
2021-03-13 22:00:32 +0200

    update loader

--- components/UI/Loader/Loader.tsx
+++ components/UI/Loader/Loader.tsx
@@ -1,18 +1,30 @@
+import { useState, useEffect } from "react"
+
 export default function Loader() {
+    const [loaderText, setloaderText] = useState("undefined")
+
 
     const phrases = [
         "Adding some more salt...",
         "This needs more chilli...",
         "Spotify: Play Fleetwood",
-        "This isn't yellow enough..."
+        "There's nothing like a crunchy salad",
     ]
 
-    const loaderText = phrases[Math.floor(Math.random() * phrases.length)];
+    function chooseRandom(): string {
+        return phrases[Math.floor(Math.random() * phrases.length)];
+    }
+
+
+    useEffect(() => {
+        setloaderText(chooseRandom())
+    })
+
 
     return (
         <div className="loader">
             <img src="/images/loader.svg" alt="" />
-            <h3>{loaderText}</h3>
+            <h3>{loaderText ? loaderText : null}</h3>
         </div>
     )
 }
