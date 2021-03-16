c9999fbd512c5040efeea68816b8bd5d7352a7aa
Arno van Staden <arnovanstaden@gmail.com>
2021-03-16 11:57:43 +0200

    update sw

--- next.config.js
+++ next.config.js
@@ -4,12 +4,8 @@ const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
     pwa: {
-        disable: process.env.NEXT_PUBLIC_ENVIRONMENT === 'development',
         dest: 'public',
         runtimeCaching,
-        register: true,
-        scope: '/',
-        sw: 'service-worker.js'
     },
     images: {
         domains: ['res.cloudinary.com'],
