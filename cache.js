f752bd6a6d4dda9e05db060466cc35e162844428
Arno van Staden <arnovanstaden@gmail.com>
2021-03-18 07:22:58 +0200

    update sw

--- cache.js
+++ cache.js
@@ -49,5 +49,16 @@ module.exports = [
             },
             networkTimeoutSeconds: 10
         }
-    }
+    },
+    {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+            cacheName: 'static-image-assets',
+            expiration: {
+                maxEntries: 64,
+                maxAgeSeconds: 24 * 60 * 60 // 24 hours
+            }
+        }
+    },
 ]
