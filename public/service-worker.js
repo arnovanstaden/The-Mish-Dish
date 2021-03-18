20aeee4693177968ea2b7daf4b9c8f99806e0bbc
Arno van Staden <arnovanstaden@gmail.com>
2021-03-18 12:56:04 +0200

    remove notifications from sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -31,43 +31,43 @@ self.addEventListener('fetch', function (event) {
 
 // Notifications
 
-self.addEventListener('push', function (e) {
-    console.log(e.data)
-    const notification = {
-        title: "New Recipes",
-        text: "You will be notified when new recipes are added to The Mish Dish!",
-    }
-    e.waitUntil(
-        displayNotification(notification)
-    );
-});
+// self.addEventListener('push', function (e) {
+//     console.log(e.data)
+//     const notification = {
+//         title: "New Recipes",
+//         text: "You will be notified when new recipes are added to The Mish Dish!",
+//     }
+//     e.waitUntil(
+//         displayNotification(notification)
+//     );
+// });
 
-self.addEventListener('notificationclick', function (e) {
-    const notification = e.notification;
-    const primaryKey = notification.data.primaryKey;
-    const action = e.action;
+// self.addEventListener('notificationclick', function (e) {
+//     const notification = e.notification;
+//     const primaryKey = notification.data.primaryKey;
+//     const action = e.action;
 
-    // if (action === 'close') {
-    //     notification.close();
-    // } else {
-    //     console.log("opening new recipe")
-    //     clients.openWindow('http://www.example.com');
-    //     notification.close();
-    // }
-});
+//     // if (action === 'close') {
+//     //     notification.close();
+//     // } else {
+//     //     console.log("opening new recipe")
+//     //     clients.openWindow('http://www.example.com');
+//     //     notification.close();
+//     // }
+// });
 
-function displayNotification(notification) {
-    const options = {
-        body: notification.text,
-        badge: '/images/favicon.png',
-        icon: '/images/icon.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-        },
-    };
+// function displayNotification(notification) {
+//     const options = {
+//         body: notification.text,
+//         badge: '/images/favicon.png',
+//         icon: '/images/icon.png',
+//         vibrate: [100, 50, 100],
+//         data: {
+//             dateOfArrival: Date.now(),
+//             primaryKey: 1
+//         },
+//     };
 
-    notification.action ? options[actions] === notification.action : null;
-    self.registration.showNotification(notification.title, options)
-}
+//     notification.action ? options[actions] === notification.action : null;
+//     self.registration.showNotification(notification.title, options)
+// }
